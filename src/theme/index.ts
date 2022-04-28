10da6e03b5fc7245629305effef73e7fed1fd971
Addison Dalton <zarch14@gmail.com>
2022-04-27 20:41:12 -0400

    small light mode style changes

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,28 @@
 import { extendTheme, type Theme, type ThemeOverride } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
-import styles from './styles';
+import themeStyles from './styles';
 import config from './config';
 
 const overrides: ThemeOverride = {
-  ...styles,
-  config,
+  ...themeStyles,
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: mode('blackAlpha.50', 'gray.800')(props)
+      }
+    })
+  },
+  components: {
+    Button: {
+      variants: {
+        solid: (props) => ({
+          bg: mode('gray.200', 'whiteAlpha.200')(props)
+        })
+      }
+    },
+  },
+  config
 };
 
 export default extendTheme(overrides) as Theme;
