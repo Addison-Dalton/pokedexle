5651b38fab41b80c39df910d978be8b67c8897a8
Addison Dalton <zarch14@gmail.com>
2022-07-19 20:13:25 -0400

    apply maxWidth to navbar

--- src/scenes/navbar/navbar.tsx
+++ src/scenes/navbar/navbar.tsx
@@ -1,27 +1,29 @@
-import { Flex, HStack, Heading } from '@chakra-ui/react';
+import { Box, Flex, HStack, Heading } from '@chakra-ui/react';
 
 import Settings from '../settings-modal';
 import IntroModal from '../intro-modal';
 
 const Navbar = () => {
   return (
-    <Flex p={2} borderBottom="solid 1px darkgrey" marginBottom={1} width="100%">
-      <HStack flex="1 1 0%" spacing={10}>
-        <IntroModal />
-      </HStack>
-      <Heading
-        flex="1 1 0%"
-        textAlign="center"
-        fontWeight={400}
-        fontFamily="'Josefin Sans', sans-serif"
-        letterSpacing="0.2rem"
-      >
-        {'Pokédexle'}
-      </Heading>
-      <HStack flex="1 1 0%" spacing={10} justifyContent="flex-end">
-        <Settings />
-      </HStack>
-    </Flex>
+    <Box p={2} borderBottom="solid 1px darkgrey" marginBottom={1} width="100%">
+      <Flex maxWidth="600px" margin="0 auto">
+        <HStack flex="1 1 0%" spacing={10}>
+          <IntroModal />
+        </HStack>
+        <Heading
+          flex="1 1 0%"
+          textAlign="center"
+          fontWeight={400}
+          fontFamily="'Josefin Sans', sans-serif"
+          letterSpacing="0.2rem"
+        >
+          {'Pokédexle'}
+        </Heading>
+        <HStack flex="1 1 0%" spacing={10} justifyContent="flex-end">
+          <Settings />
+        </HStack>
+      </Flex>
+    </Box>
   );
 };
 
