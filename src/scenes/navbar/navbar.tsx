fd26d0bb3ef0fa3394d5c10d3f10f6a0ad6b2002
Addison Dalton <zarch14@gmail.com>
2022-07-19 23:33:23 -0400

    improved navbar element spacing at mobile

--- src/scenes/navbar/navbar.tsx
+++ src/scenes/navbar/navbar.tsx
@@ -9,20 +9,22 @@ const Navbar = () => {
   return (
     <Box p={2} borderBottom="solid 1px darkgrey" marginBottom={1} width="100%">
       <Flex maxWidth="600px" margin="0 auto">
-        <HStack flex="1 1 0%" spacing={3}>
+        <HStack flex="1 1 0%" spacing={2}>
           <IntroModal />
           <AboutModal />
         </HStack>
         <Heading
           flex="1 1 0%"
+          marginRight={2}
+          marginLeft={2}
           textAlign="center"
           fontWeight={400}
           fontFamily="'Josefin Sans', sans-serif"
-          letterSpacing="0.2rem"
+          letterSpacing={[0, '0.2rem']}
         >
           {'Pokédexle'}
         </Heading>
-        <HStack flex="1 1 0%" spacing={3} justifyContent="flex-end">
+        <HStack flex="1 1 0%" spacing={2} justifyContent="flex-end">
           <StatsModal />
           <SettingsModal />
         </HStack>
